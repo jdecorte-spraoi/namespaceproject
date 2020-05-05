f3cc22586cc42669d52c19d766cae08fb0188b9a
Jason DeCorte <jason@spraoi.ai>
2020-05-05 19:19:05 -0400

    streamlined plugin code

--- src/pkg_core/scatpack/core/main.py
+++ src/pkg_core/scatpack/core/main.py
@@ -2,19 +2,10 @@ import pkgutil
 import importlib
 import scatpack.plugin
 
-
-def iter_namespace(ns_pkg):
-    # Specifying the second argument (prefix) to iter_modules makes the
-    # returned name an absolute name instead of a relative one. This allows
-    # import_module to work without having to do additional modification to
-    # the name.
-    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
-
-
-discovered_plugins = {
+plugins = {
     name: importlib.import_module(name)
     for finder, name, ispkg
-    in iter_namespace(scatpack.plugin)
+    in pkgutil.iter_modules(scatpack.plugin.__path__, scatpack.plugin.__name__ + ".")
 }
 
 
@@ -25,12 +16,12 @@ def summer(x, y):
 def run(arg1, arg2):
     print(f"Name  = Core")
     print(f"Value = {summer(arg1, arg2)}")
-    if 'scatpack.plugin.plugin_a' in discovered_plugins:
-        plugin_a = discovered_plugins['scatpack.plugin.plugin_a']
+    if 'scatpack.plugin.plugin_a' in plugins:
+        plugin_a = plugins['scatpack.plugin.plugin_a']
         print(f"Name = {plugin_a.name}")
         print(f"Value= {plugin_a.multiplier(arg1, arg2)}")
-    if 'scatpack.plugin.plugin_b' in discovered_plugins:
-        plugin_b = discovered_plugins['scatpack.plugin.plugin_b']
+    if 'scatpack.plugin.plugin_b' in plugins:
+        plugin_b = plugins['scatpack.plugin.plugin_b']
         print(f"Name = {plugin_b.name}")
         print(f"Value= {plugin_b.subtractor(arg1, arg2)}")
     print("finished")
